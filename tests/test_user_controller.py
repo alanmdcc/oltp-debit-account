bcfa52e5190d3cddc9d9cce2c27191f6912fe067
Alan Martin del Campo Cervantes <Alan.MartinDelCampo.C@ibm.com>
2023-08-15 19:46:07 -0600

    Revert "add tests for user controller"
    This reverts commit 458c5630661b30557539479a4da649c7894ff06e.

--- tests/test_user_controller.py
+++ tests/test_user_controller.py
@@ -12,28 +12,3 @@ def test_create_user():
     print(user_test_from_db)
     assert user_test_from_db.age == 20
     assert user_test_from_db.name == 'Alan'
-
-def test_get_user_by_id():
-    User.delete().execute()
-    user_test = User(age=20, name='Alan')
-    user_test.save()
-    user_from_db = UserController.get_user_by_id(1)
-    assert user_from_db.age == 20
-    assert user_from_db.name == 'Alan'
-
-def test_get_user_by_name():
-    User.delete().execute()
-    user_test = User(age=20, name='Alan')
-    user_test.save()
-    user_from_db = UserController.get_user_by_name('Alan')
-    assert user_from_db.age == 20
-    assert user_from_db.name == 'Alan'
-
-def test_update_name():
-    User.delete().execute()
-    user_test = User(age=20, name='Alan')
-    user_test.save()
-    UserController.update_name(user_test, 'Aldo')
-    user_from_db = User.get(name='Aldo')
-    assert user_from_db.age == 20
-    assert user_from_db.name == 'Aldo'
